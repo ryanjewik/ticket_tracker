de77b6591d378a2a692b11a48d318f0f1eeadbc4
ryanjewik <jewik@chapman.edu>
2025-10-10 19:36:11 -0700

    setup spring boot app

---
+++ src/main/java/com/tickettracker/TickettrackerApplication.java
@@ -0,0 +1,14 @@
+package com.tickettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickettrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickettrackerApplication.class, args);
+		System.out.println("Server is running on http://localhost:8080");
+	}
+
+}
---
+++ src/test/java/com/tickettracker/TickettrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickettrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
